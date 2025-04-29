6e0e3d2f1013f447777c1a4651f600767dc1ee50
Fleurdel34 <betdevelweb0953@gmail.com>
2025-04-29 16:18:29 +0200

    update the differents paths

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
-import { Component, OnInit } from '@angular/core';
-import { FaceSnapListComponent } from "./face-snap-list/face-snap-list.component";
+import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
+import { RouterOutlet } from '@angular/router';
 
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [ FaceSnapListComponent, HeaderComponent],
+  imports: [HeaderComponent, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
